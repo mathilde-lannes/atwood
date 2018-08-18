af2a39ca42e07ebc5fbe314373824bbd0f1af21e
matylannes <mathilde.lannes@gmail.com>
2018-08-18 15:05:22 +0200

    Add "add" and "open" buttons to edit a MarkDown file

---
+++ src/app/main/app.module.ts
@@ -0,0 +1,42 @@
+import 'zone.js/dist/zone-mix';
+import 'reflect-metadata';
+import '../../polyfills';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+// NG Translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { AppComponent } from './app.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../../assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    })
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
